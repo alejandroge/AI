20d677444e648f901f8b5b03053c443c66e7db97
Alejandro <alejandro.guevara.esc@gmail.com>
2017-02-14 10:48:48 -0600

    Final corrections to the code

--- Structures.c
+++ Structures.c
@@ -9,13 +9,13 @@ typedef struct{
 
 MATRIX * allocateMatrix(unsigned char *N, unsigned char *M);
 void deallocateMatrix(MATRIX* ptr);
-void insertData(MATRIX* ptr, unsigned char *N, unsigned char *M);
-void printMatrix(MATRIX* ptr, unsigned char *N, unsigned char *M);
-void transposeMatrix(MATRIX* ptr1, MATRIX* ptr2, unsigned char *N, unsigned char *M);
+void insertData(MATRIX* ptr);
+void printMatrix(MATRIX* ptr);
+void transposeMatrix(MATRIX* ptr1, MATRIX* ptr2);
 
 int main(int argc, char const *argv[]) {
   MATRIX *pM, *pMT;
-  unsigned char N,M;
+  unsigned char N, M;
 
   printf("Ingrese las dimensiones de la matriz\n\tN: ");
   scanf("%hhu", &N);
@@ -24,12 +24,12 @@ int main(int argc, char const *argv[]) {
 
   pM = allocateMatrix(&N, &M);
   pMT = allocateMatrix(&M, &N);
-  insertData(pM, &N, &M);
+  insertData(pM);
   printf("\n");
-  printMatrix(pM, &N, &M);
+  printMatrix(pM);
   printf("\nLa matriz transpuesta es:\n");
-  transposeMatrix(pM, pMT, &N, &M);
-  printMatrix(pMT, &M, &N);
+  transposeMatrix(pM, pMT);
+  printMatrix(pMT);
   deallocateMatrix(pM);
   deallocateMatrix(pMT);
   return 0;
@@ -39,6 +39,8 @@ MATRIX * allocateMatrix(unsigned char *N, unsigned char *M){
   MATRIX *ptr;
   ptr = (MATRIX *)malloc(sizeof(MATRIX));
   ptr->data = (float *)malloc((*N)*(*M)*sizeof(float));
+  ptr->N = *N;
+  ptr->M = *M;
   return ptr;
 }
 
@@ -47,27 +49,27 @@ void deallocateMatrix(MATRIX* pM) {
   free(pM);
 }
 
-void insertData(MATRIX *ptr, unsigned char *N, unsigned char *M) {
+void insertData(MATRIX *ptr) {
   unsigned char i, j;
-  for (i = 0; i < *N; i++)
-  for (j = 0; j < *M; j++) {
+  for (i = 0; i < ptr->N; i++)
+  for (j = 0; j < ptr->M; j++) {
     printf("\tdata[%hhu][%hhu] = ", i+1, j+1);
-    scanf("%f", ptr->data+i*(*M)+j);
+    scanf("%f", ptr->data+i*(ptr->M)+j);
   }
 }
 
-void printMatrix(MATRIX* ptr, unsigned char *N, unsigned char *M) {
+void printMatrix(MATRIX* ptr) {
   unsigned char i, j;
-  for (i = 0; i < *N; i++) {
-    for (j = 0; j < *M; j++)
-      printf("\tdata[%hhu][%hhu] = %g", i+1, j+1, ptr->data[i*(*M)+j]);
+  for (i = 0; i < ptr->N; i++) {
+    for (j = 0; j < ptr->M; j++)
+      printf("\tdata[%hhu][%hhu] = %g", i+1, j+1, ptr->data[i*ptr->M+j]);
     printf("\n");
   }
 }
 
-void transposeMatrix(MATRIX* ptr1, MATRIX* ptr2, unsigned char *N, unsigned char *M) {
+void transposeMatrix(MATRIX* ptr1, MATRIX* ptr2) {
   unsigned char i, j;
-  for (i = 0; i < *N; i++)
-  for (j = 0; j < *M; j++)
-    ptr2->data[j*(*N)+i] = ptr1->data[i*(*M)+j];
+  for (i = 0; i < ptr1->N; i++)
+  for (j = 0; j < ptr1->M; j++)
+    ptr2->data[j*ptr1->N+i] = ptr1->data[i*ptr1->M+j];
 }
